ea88cc936fa723ca9c371cef9502d582eed246bc
roee88 <roee88@gmail.com>
2021-07-11 15:55:20 +0300

    Added taxonomy compile command
    Signed-off-by: roee88 <roee88@gmail.com>

---
+++ pkg/text/indent.go
@@ -0,0 +1,21 @@
+package text
+
+import (
+	"strings"
+)
+
+// indents a block of text with an indent string
+func Indent(text, indent string) string {
+	if text[len(text)-1:] == "\n" {
+		result := ""
+		for _, j := range strings.Split(text[:len(text)-1], "\n") {
+			result += indent + j + "\n"
+		}
+		return result
+	}
+	result := ""
+	for _, j := range strings.Split(strings.TrimRight(text, "\n"), "\n") {
+		result += indent + j + "\n"
+	}
+	return result[:len(result)-1]
+}
